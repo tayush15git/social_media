db309786b92ace70ebb3c3c413582f7a3bc3d794
Ayush Tiwari <atfromknp01@gmail.com>
2025-03-09 02:29:47 +0530

    added callback url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.allowed_request_methods = [:get, :post]
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
+           callback_url: ENV['TWITTER_CALLBACK_URL']
 end
 
